d1d788a19224813f3ebe1e9ad5c44a1ef173bd31
Utku Turna <turnautku@gmail.com>
2022-10-20 15:12:59 +0200

    feat: add simple head section to _document update favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="This is an example project to list houses"
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from 'next';
 import MainLayout from '../components/layouts/main/MainLayout';
-import Button from '../components/ui/button/Button';
 import { MOCK_CITIES } from '../lib/data/mock.cities';
 import CityLink from '../components/partials/CityLink';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <MainLayout>
+      <Head>
+        <title>Listing Houses</title>
+      </Head>
       <div className="py-4">
         <h1 className="page-title">Select a city to live 🥰</h1>
         <div className="grid md:grid-cols-2 grid-cols-1 gap-3">
